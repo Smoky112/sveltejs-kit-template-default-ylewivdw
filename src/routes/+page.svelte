82382b527e70351d32907a59f9c50caa1b76c4ad
Smoky112 <git@stackblitz.com>
2024-12-16 11:31:27 +0100

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<style>
+  h1 {
+    color: blue;
+  }
+  a {
+    color: red;
+  }
+</style>
+
+<script>
+  import componente1 from '$lib/componente1.svelte';  let name = "Davide";
+</script>
+
+<p><a href="/firstPage">Ciao {name}</a></p>
+
+<componente1/>
---
+++ src/routes/firstPage/+page.svelte
@@ -0,0 +1,5 @@
+<h1> Ciao griotti </h1>
+
+<p>
+<a href="/">Vai alla home </a>
+</p>
